c994611c5204cfa61aa7073acac326e6fa917259
Mohammed Essa <moessa089@gmail.com>
2024-07-21 11:47:34 +0000

    Change setting.py to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ik$@v+$3k#eveae3#a79+c6y3($cj+xlbifhkyh5gps$30cros'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-moessa1-djangoblog-z0ny3vo3lr6.ws.codeinstitute-ide.net',
