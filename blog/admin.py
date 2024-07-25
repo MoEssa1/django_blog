057f7e7d19055dd2b3965c546dea0dc46979d716
Mohammed Essa <moessa089@gmail.com>
2024-07-25 19:00:21 +0000

    Add suppernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post) # this is no longer needed as we've now used a decorator to register the class on line6
 admin.site.register(Comment)
